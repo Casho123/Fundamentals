c930dc5d079d6a03aa85667c5c5397ae0867ca5a
My PC <alex.petkov606@gmail.com>
2022-11-20 21:53:25 +0200

    fds

--- src/Problem8/Problem8.java
+++ src/Problem8/Problem8.java
@@ -8,6 +8,12 @@ public class Problem8 {
         int n = Integer.parseInt(scan.nextLine());
 
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
 
+        }
     }
 }
