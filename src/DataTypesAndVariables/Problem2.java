b6cfdb636dcb7019787169c5c38310f085f35016
My PC <alex.petkov606@gmail.com>
2022-11-21 20:55:29 +0200

    fds

--- src/DataTypesAndVariables/Problem2.java
+++ src/DataTypesAndVariables/Problem2.java
@@ -1,4 +1,19 @@
 package DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int number = Integer.parseInt(scan.nextLine());
+
+        String num = number + "";
+        int sum = 0;
+        for (int i = 0; i < num.length(); i++) {
+            sum+= Integer.parseInt(num.charAt(i) - 48 + "");
+
+        }
+        System.out.println(sum);
+    }
 }
