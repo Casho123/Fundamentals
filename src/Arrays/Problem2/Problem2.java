d0b47fccb94b1394b68a0c27b0180057550555ae
My PC <alex.petkov606@gmail.com>
2022-11-24 23:45:05 +0200

    fds

--- src/Arrays/Problem2/Problem2.java
+++ src/Arrays/Problem2/Problem2.java
@@ -1,4 +1,21 @@
 package Arrays.Problem2;
 
+import java.util.Scanner;
+
 public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] arr1 = scan.nextLine().split(" ");
+        String[] arr2 = scan.nextLine().split(" ");
+
+
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                if (arr1[j].equals(arr2[i])) {
+                    System.out.print(arr2[i] + " ");
+                }
+            }
+        }
+    }
 }
