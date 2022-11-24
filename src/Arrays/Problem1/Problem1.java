33560a949dc782e520a58ca2c6ee3448d3cd1ca2
My PC <alex.petkov606@gmail.com>
2022-11-24 23:18:40 +0200

    fds

---
+++ src/Arrays/Problem1/Problem1.java
@@ -0,0 +1,9 @@
+package Arrays.Problem1;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
