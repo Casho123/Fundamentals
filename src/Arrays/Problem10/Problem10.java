b8f25c911191116c64b0e767825e8f13fe996e1a
My PC <alex.petkov606@gmail.com>
2022-12-02 00:21:25 +0200

    fds

---
+++ src/Arrays/Problem10/Problem10.java
@@ -0,0 +1,53 @@
+package Arrays.Problem10;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] chest = scan.nextLine().split("\\|");
+
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("Yohoho!")) {
+                break;
+            }
+            String[] elements = input.split("\\s+");
+            String command = elements[0];
+
+            switch (command) {
+                case "Loot":
+                    for (int i = 1; i < elements.length ; i++) {
+                        boolean isContained = false;
+                        for (int j = 0; j < chest.length; j++) {
+                            if (chest[j].equals(elements[i])) {
+                                isContained = true;
+                                break;
+                            }
+                        }
+                        if (!isContained) {
+                            String currentChest = elements[i] + " " + String.join(" ", chest);
+                            chest = currentChest.split("\\s+");
+                        }
+                    }
+                    break;
+                case "Drop":
+                    int index = Integer.parseInt(elements[1]);
+                    if (index >= 0 && index < chest.length) {
+                        String temp = chest[index];
+                        chest[index] = chest[chest.length-1];
+                        chest[chest.length-1] = temp;
+
+
+                    }
+                    break;
+                case "Steal":
+                    break;
+            }
+
+        }
+
+
+    }
+}
