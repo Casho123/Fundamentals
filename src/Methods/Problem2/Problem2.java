367e4b3c64477751f1beb4e22cbe9b1f6bbe5fe0
My PC <alex.petkov606@gmail.com>
2022-12-03 16:01:15 +0200

    fds

--- src/Methods/Problem2/Problem2.java
+++ src/Methods/Problem2/Problem2.java
@@ -5,5 +5,25 @@ import java.util.Scanner;
 public class Problem2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String word = scan.nextLine();
+
+        vowelsCount(word);
+
+
+    }
+
+    public static void vowelsCount(String word) {
+        int counter = 0;
+        String vowels = "aoueiyAOUEIY";
+        for (int i = 0; i < word.length(); i++) {
+            String current = word.charAt(i) + "";
+            if (vowels.contains(current)) {
+                counter++;
+            }
+
+        }
+
+        System.out.println(counter);
     }
 }
