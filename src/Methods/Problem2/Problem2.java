6a78227b4983c7b805a76f2fea2e87d25f273e8e
My PC <alex.petkov606@gmail.com>
2022-12-03 15:26:44 +0200

    fds

---
+++ src/Methods/Problem2/Problem2.java
@@ -0,0 +1,9 @@
+package Methods.Problem2;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
