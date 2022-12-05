8f9cf70e55251f42dec41389b926019d9535973a
My PC <alex.petkov606@gmail.com>
2022-12-05 13:08:55 +0200

    fds

--- src/Methods/Problem9.java
+++ src/Methods/Problem9.java
@@ -6,20 +6,24 @@ public class Problem9 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
-    }
-    public static void isPalindrome(String input, Scanner scan) {
         while (true) {
-            input = scan.nextLine();
+            String input = scan.nextLine();
             if (input.equals("END")) {
                 break;
             }
+            isPalindrome(input);
+        }
+
+
+    }
+    public static void isPalindrome(String input) {
+
             StringBuilder reversed = new StringBuilder();
             for (int i = input.length()-1; i >= 0; i--) {
                 reversed.append(input.charAt(i));
 
             }
             System.out.println(reversed.toString().equals(input));
-        }
+
     }
 }
