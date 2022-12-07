42284eb5d8d9ea738d43ddf71e5e594e3e29d379
My PC <alex.petkov606@gmail.com>
2022-12-07 15:04:42 +0200

    fds

---
+++ src/Methods/MoreExercise/Problem2/Problem2.java
@@ -0,0 +1,11 @@
+package Methods.MoreExercise.Problem2;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
