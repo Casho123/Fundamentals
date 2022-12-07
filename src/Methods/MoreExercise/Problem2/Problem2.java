80c272383ec3e37a0b653f00405f79f237d91657
My PC <alex.petkov606@gmail.com>
2022-12-07 15:10:20 +0200

    fds

--- src/Methods/MoreExercise/Problem2/Problem2.java
+++ src/Methods/MoreExercise/Problem2/Problem2.java
@@ -6,6 +6,26 @@ public class Problem2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        double first = Double.parseDouble(scan.nextLine());
+        double second = Double.parseDouble(scan.nextLine());
+        double third = Double.parseDouble(scan.nextLine());
+        double fourth = Double.parseDouble(scan.nextLine());
 
+        getClosestPoint(first, second, third, fourth);
+
+
+
+
+    }
+    public static void getClosestPoint(double first, double second, double third, double fourth) {
+
+        double firstResult = Math.max(Math.abs(first), Math.abs(second));
+        double secondResult = Math.max(Math.abs(third), Math.abs(fourth));
+
+        if (firstResult <= secondResult) {
+            System.out.printf("(%.0f, %.0f)", first, second);
+        } else {
+            System.out.printf("(%.0f, %.0f)", third, fourth);
+        }
     }
 }
