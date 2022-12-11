fed906494de9e9fe6e30a69e3e977b46937b3e50
My PC <alex.petkov606@gmail.com>
2022-12-11 14:14:23 +0200

    fds

---
+++ src/Lists/CardsGame/CardsGame.java
@@ -0,0 +1,10 @@
+package Lists.CardsGame;
+
+import java.util.Scanner;
+
+public class CardsGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
