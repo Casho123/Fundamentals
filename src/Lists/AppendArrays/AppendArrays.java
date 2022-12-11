950c79160922bb10b8c95b4457cbc81b7f5f44be
My PC <alex.petkov606@gmail.com>
2022-12-11 14:31:46 +0200

    fds

---
+++ src/Lists/AppendArrays/AppendArrays.java
@@ -0,0 +1,28 @@
+package Lists.AppendArrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AppendArrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] arr = scan.nextLine().split("\\|");
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = arr.length-1; i >= 0 ; i--) {
+            String value = arr[i];
+            String[] currentArr = value.split("\\s+");
+            for (int j = 0; j < currentArr.length; j++) {
+                if (!currentArr[j].equals("")) {
+                    list.add(Integer.parseInt(currentArr[j]));
+                }
+            }
+        }
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+
+        }
+    }
+}
