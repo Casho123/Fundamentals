28294a0ac5146f685b6c86bc794ef1c91da220cc
My PC <alex.petkov606@gmail.com>
2022-12-14 14:28:20 +0200

    fds

---
+++ src/Lists/MoreExercises/DrumSet/DrumSet.java
@@ -0,0 +1,11 @@
+package Lists.MoreExercises.DrumSet;
+
+import java.util.Scanner;
+
+public class DrumSet {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
