e538c7939e618e3008f30a480beeec54f3f75d32
My PC <alex.petkov606@gmail.com>
2022-12-23 13:54:22 +0200

    fds

--- src/Maps/CountCharsInAString/CountCharsInAString.java
+++ src/Maps/CountCharsInAString/CountCharsInAString.java
@@ -1,6 +1,7 @@
 package Maps.CountCharsInAString;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,21 +9,30 @@ public class CountCharsInAString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String input = scan.nextLine().replace(" ", "");
+        String input = scan.nextLine();
 
-        Map<Character, Integer> chars = new HashMap<>();
+        Map<Character, Integer> chars = new LinkedHashMap<>();
 
-        for (int i = 0; i < input.length(); i++) {
-            char current = input.charAt(i);
-            if (!chars.containsKey(current)) {
-                chars.put(current, 0);
+        char[] arr = input.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            char current = arr[i];
+            if (current != ' ') {
+                if (!chars.containsKey(current)) {
+                    chars.put(current, 1);
+                } else {
+                    chars.put(current, chars.get(current) + 1);
+                }
+
+
             }
-            chars.put(current, chars.get(current) + 1);
+
         }
 
         for (Map.Entry<Character, Integer> entry : chars.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
 
         }
+
     }
 }
