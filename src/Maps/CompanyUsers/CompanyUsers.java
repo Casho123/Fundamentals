7124aaf4bc18753fd9df26492f37cd4380f852f7
My PC <alex.petkov606@gmail.com>
2022-12-26 23:00:47 +0200

    fds

---
+++ src/Maps/CompanyUsers/CompanyUsers.java
@@ -0,0 +1,40 @@
+package Maps.CompanyUsers;
+
+import java.util.*;
+
+public class CompanyUsers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Map<String, List<String>>  companies = new LinkedHashMap<>();
+
+        while (true) {
+
+            String input = scan.nextLine();
+            if (input.equals("End")) {
+                break;
+            }
+            String[] elements = input.split(" -> ");
+            String company = elements[0];
+            String id = elements[1];
+
+            if (!companies.containsKey(company)) {
+                companies.put(company, new ArrayList<>());
+            }
+
+            if (!companies.get(company).contains(id)) {
+                companies.get(company).add(id);
+            }
+
+        }
+
+        for (Map.Entry<String, List<String>> entry : companies.entrySet()) {
+            System.out.println(entry.getKey());
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                System.out.println("-- " + entry.getValue().get(i));
+
+            }
+
+        }
+    }
+}
