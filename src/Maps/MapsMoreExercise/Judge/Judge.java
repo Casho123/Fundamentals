806753f5915e788876020f3b9cef362c2601f470
My PC <alex.petkov606@gmail.com>
2023-01-01 20:54:01 +0200

    fds

---
+++ src/Maps/MapsMoreExercise/Judge/Judge.java
@@ -0,0 +1,11 @@
+package Maps.MapsMoreExercise.Judge;
+
+import java.util.Scanner;
+
+public class Judge {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
