393d3c2c3d69423dd31bb85a84ce049a4167971d
My PC <alex.petkov606@gmail.com>
2023-01-01 21:20:15 +0200

    fds

--- src/Maps/MapsMoreExercise/Judge/Judge.java
+++ src/Maps/MapsMoreExercise/Judge/Judge.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Judge {
     public static void main(String[] args) {
@@ -34,18 +35,44 @@ public class Judge {
             }
 
         }
-        for (Map.Entry<String, Map<String, Integer>> entry : contests.entrySet()) {
-            System.out.printf("%s: %d participants\n", entry.getKey(), entry.getValue().size());
-            int counter = 1;
-            for (Map.Entry<String, Integer> contestant : entry.getValue().entrySet()) {
-                System.out.printf("%d. %s <::> %d\n", counter, contestant.getKey(), contestant.getValue());
-                counter++;
+        AtomicInteger num = new AtomicInteger(1);
+
+        contests.entrySet().stream().forEach(e -> {
+            num.set(1);
+            System.out.println(e.getKey() + ": " + e.getValue().size() + " participants");
+            e.getValue().entrySet().stream().sorted((k2, k1) -> {
+                int sort = Integer.compare(k1.getValue(), k2.getValue());
+                if (sort == 0) {
+                    sort = k2.getKey().compareTo(k1.getKey());
+
+                }
+                return sort;
+            }).forEach(element -> {
+                System.out.printf("%d. %s <::> %d\n", num.getAndIncrement(), element.getKey(), element.getValue());
+            });
+        });
+
+        Map<String, Integer> standings = new LinkedHashMap<>();
+
+        for(Map.Entry<String, Map<String, Integer>> contest : contests.entrySet()) {
+            for (Map.Entry<String, Integer> value : contest.getValue().entrySet()) {
+                standings.putIfAbsent(value.getKey(), 0);
+                standings.put(value.getKey(), standings.get(value.getKey()) + value.getValue());
             }
-
         }
+        System.out.println("Individual standings:");
+        num.set(1);
 
+        standings.entrySet().stream().sorted((e2, e1) -> {
+            int sort = Integer.compare(e1.getValue(), e2.getValue());
+            if (sort == 0) {
+                sort = e2.getKey().compareTo(e1.getKey());
+            }
+            return sort;
 
-
+        }).forEach(element -> {
+            System.out.printf("%d. %s -> %d\n", num.getAndIncrement(), element.getKey(), element.getValue());
+        });
 
 
     }
