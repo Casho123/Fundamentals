107de43e000c59b236c250d019ca3c6de6e3317f
My PC <alex.petkov606@gmail.com>
2023-01-02 22:35:40 +0200

    fds

--- src/Maps/MapsMoreExercise/MOBAChallenger/MOBAChallenger.java
+++ src/Maps/MapsMoreExercise/MOBAChallenger/MOBAChallenger.java
@@ -1,10 +1,44 @@
 package Maps.MapsMoreExercise.MOBAChallenger;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class MOBAChallenger {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Map<String, Map<String, Integer>> players = new TreeMap<>();
+
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("Season end")) {
+
+            }
+            if (!input.contains("vs")) {
+                String[] elements = input.split(" -> ");
+                String player = elements[0];
+                String position = elements[1];
+                int skillPoints = Integer.parseInt(elements[2]);
+                if (!players.containsKey(player)) {
+                    players.put(player, new TreeMap<>());
+                } else {
+                    if (!players.get(player).containsKey(position)) {
+                        players.get(player).put(position, skillPoints);
+                    } else {
+                        int currentPoints = players.get(player).get(position);
+                        if (currentPoints < skillPoints) {
+                            players.get(player).put(position, skillPoints);
+                        }
+                    }
+                }
+
+            } else {
+
+            }
+        }
+
 
 
     }
