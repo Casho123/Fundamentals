58fa5c48c00742ca3eced6b14f913c373ba715fe
My PC <alex.petkov606@gmail.com>
2023-01-02 22:26:36 +0200

    fds

---
+++ src/Maps/MapsMoreExercise/MOBAChallenger/MOBAChallenger.java
@@ -0,0 +1,11 @@
+package Maps.MapsMoreExercise.MOBAChallenger;
+
+import java.util.Scanner;
+
+public class MOBAChallenger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
