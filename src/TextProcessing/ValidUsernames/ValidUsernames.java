2a041484cabea3a423d6cd4b99f898214ae2734d
My PC <alex.petkov606@gmail.com>
2023-01-03 16:17:22 +0200

    fds

---
+++ src/TextProcessing/ValidUsernames/ValidUsernames.java
@@ -0,0 +1,11 @@
+package TextProcessing.ValidUsernames;
+
+import java.util.Scanner;
+
+public class ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
