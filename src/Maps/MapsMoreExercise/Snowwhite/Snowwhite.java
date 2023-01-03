27ab81c4371a2f22e021b7ac84bb8f75a2e5862b
My PC <alex.petkov606@gmail.com>
2023-01-03 15:48:19 +0200

    fds

---
+++ src/Maps/MapsMoreExercise/Snowwhite/Snowwhite.java
@@ -0,0 +1,11 @@
+package Maps.MapsMoreExercise.Snowwhite;
+
+import java.util.Scanner;
+
+public class Snowwhite {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
