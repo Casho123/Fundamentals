3e81a4c38609dd8cc8707a8831f79969bf695c5c
My PC <alex.petkov606@gmail.com>
2023-01-04 00:40:13 +0200

    fds

--- src/TextProcessing/ExtractFile/ExtractFile.java
+++ src/TextProcessing/ExtractFile/ExtractFile.java
@@ -6,5 +6,13 @@ public class ExtractFile {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        String input = scan.nextLine();
+
+        int fileBeginning = input.lastIndexOf("\\") + 1;
+        int fileEnd = input.indexOf(".");
+
+        System.out.println("File name: " + input.substring(fileBeginning, fileEnd));
+        System.out.println("File extension: " + input.substring(fileEnd+1));
+
     }
 }
