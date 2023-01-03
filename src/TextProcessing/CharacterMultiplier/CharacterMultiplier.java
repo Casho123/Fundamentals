5170438af72188f512383d8096ead2673305cba4
My PC <alex.petkov606@gmail.com>
2023-01-04 00:14:40 +0200

    fds

---
+++ src/TextProcessing/CharacterMultiplier/CharacterMultiplier.java
@@ -0,0 +1,10 @@
+package TextProcessing.CharacterMultiplier;
+
+import java.util.Scanner;
+
+public class CharacterMultiplier {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
