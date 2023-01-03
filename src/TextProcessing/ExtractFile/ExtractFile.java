e10a1dd5bf3bed8957efea0d834bd86798bd1d20
My PC <alex.petkov606@gmail.com>
2023-01-04 00:31:39 +0200

    fds

---
+++ src/TextProcessing/ExtractFile/ExtractFile.java
@@ -0,0 +1,10 @@
+package TextProcessing.ExtractFile;
+
+import java.util.Scanner;
+
+public class ExtractFile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
