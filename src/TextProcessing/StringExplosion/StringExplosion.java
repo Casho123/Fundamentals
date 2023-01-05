f0495c876e287401a9e36a674ccf955522eac836
My PC <alex.petkov606@gmail.com>
2023-01-05 21:49:48 +0200

    fds

--- src/TextProcessing/StringExplosion/StringExplosion.java
+++ src/TextProcessing/StringExplosion/StringExplosion.java
@@ -1,10 +1,41 @@
 package TextProcessing.StringExplosion;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StringExplosion {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+
+        List<Character> field = new ArrayList<>();
+
+        for (int i = 0; i < name.length(); i++) {
+            char symbol = name.charAt(i);
+            field.add(symbol);
+
+            if (symbol == '>') {
+                i++;
+                int bomb = name.charAt(i) - '0';
+
+                int j = i;
+                for (; j < i + bomb && j < name.length(); j++) {
+                    if (name.charAt(j) == '>') {
+                        field.add('>');
+                        j++;
+                        bomb += (name.charAt(j) - '0') + 1;
+                    }
+                }
+
+                i = j - 1;
+            }
+        }
+
+        for (int i = 0; i < field.size() ; i++) {
+            System.out.print(field.get(i));
+        }
 
 
     }
