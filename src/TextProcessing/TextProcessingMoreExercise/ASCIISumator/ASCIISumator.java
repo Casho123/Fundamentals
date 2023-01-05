2530f0d0b9ea157378a47911d58a933c720fcbc0
My PC <alex.petkov606@gmail.com>
2023-01-05 22:18:08 +0200

    fds

---
+++ src/TextProcessing/TextProcessingMoreExercise/ASCIISumator/ASCIISumator.java
@@ -0,0 +1,11 @@
+package TextProcessing.TextProcessingMoreExercise.ASCIISumator;
+
+import java.util.Scanner;
+
+public class ASCIISumator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
