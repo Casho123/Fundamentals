99bf1419bfb281dd2b199dcf402f5e212ad9d2e0
My PC <alex.petkov606@gmail.com>
2023-01-05 21:51:41 +0200

    fds

---
+++ src/TextProcessing/LettersChangeNumbers/LettersChangeNumbers.java
@@ -0,0 +1,11 @@
+package TextProcessing.LettersChangeNumbers;
+
+import java.util.Scanner;
+
+public class LettersChangeNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
