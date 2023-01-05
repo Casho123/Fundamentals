efd556b4d697dbe1fa9e845b968cd6242b2dfccf
My PC <alex.petkov606@gmail.com>
2023-01-05 22:22:25 +0200

    fds

--- src/TextProcessing/TextProcessingMoreExercise/ASCIISumator/ASCIISumator.java
+++ src/TextProcessing/TextProcessingMoreExercise/ASCIISumator/ASCIISumator.java
@@ -6,6 +6,22 @@ public class ASCIISumator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        char first = scan.nextLine().charAt(0);
+        char second = scan.nextLine().charAt(0);
+        String input = scan.nextLine();
+
+        int sum = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            int current = input.charAt(i);
+            if (first < current && current < second) {
+                sum += current;
+            }
+
+        }
+        System.out.println(sum);
+
+
 
     }
 }
