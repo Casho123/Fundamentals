27f8cad8c3c5ca6c633728789b8769252f3af9d8
My PC <alex.petkov606@gmail.com>
2023-01-05 22:11:36 +0200

    fds

---
+++ src/TextProcessing/TextProcessingMoreExercise/ExtractPersonInformation/ExtractPersonInfo.java
@@ -0,0 +1,11 @@
+package TextProcessing.TextProcessingMoreExercise.ExtractPersonInformation;
+
+import java.util.Scanner;
+
+public class ExtractPersonInfo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
