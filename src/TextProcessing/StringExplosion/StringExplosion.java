b87a6966c57468273aaf8b049505a09b75a935f5
My PC <alex.petkov606@gmail.com>
2023-01-05 21:43:08 +0200

    fds

---
+++ src/TextProcessing/StringExplosion/StringExplosion.java
@@ -0,0 +1,11 @@
+package TextProcessing.StringExplosion;
+
+import java.util.Scanner;
+
+public class StringExplosion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
