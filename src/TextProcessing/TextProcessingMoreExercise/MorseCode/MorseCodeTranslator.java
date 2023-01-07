aadce32240afbef257f5f9bd74389481e86a3b06
My PC <alex.petkov606@gmail.com>
2023-01-07 22:38:54 +0200

    fds

---
+++ src/TextProcessing/TextProcessingMoreExercise/MorseCode/MorseCodeTranslator.java
@@ -0,0 +1,11 @@
+package TextProcessing.TextProcessingMoreExercise.MorseCode;
+
+import java.util.Scanner;
+
+public class MorseCodeTranslator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
