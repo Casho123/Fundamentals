e0006b763e8269fe39474e77eef73327716f21b8
My PC <alex.petkov606@gmail.com>
2023-01-08 20:12:04 +0200

    fds

--- src/Advanced/StacksAndQueues/ReverseNumbersWithAStack/ReverseNumbers.java
+++ src/Advanced/StacksAndQueues/ReverseNumbersWithAStack/ReverseNumbers.java
@@ -1,10 +1,21 @@
 package Advanced.StacksAndQueues.ReverseNumbersWithAStack;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < numbers.length; i++) {
+            stack.push(numbers[i]);
+        }
+        while(!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
 
 
     }
