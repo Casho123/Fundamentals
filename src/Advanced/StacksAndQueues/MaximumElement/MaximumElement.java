2b3899ecaf7fcda8a3937f3174916c8a2ee2d775
My PC <alex.petkov606@gmail.com>
2023-01-11 17:06:48 +0200

    fds

--- src/Advanced/StacksAndQueues/MaximumElement/MaximumElement.java
+++ src/Advanced/StacksAndQueues/MaximumElement/MaximumElement.java
@@ -1,10 +1,34 @@
 package Advanced.StacksAndQueues.MaximumElement;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class MaximumElement {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            int[] elements = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+            int command = elements[0];
+            switch (command) {
+                case 1:
+                    stack.push(elements[1]);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    Object max = Collections.max(stack);
+                    System.out.println(max);
+                    break;
+            }
+
+
+        }
 
 
     }
