ee2ebe6a1495f9de6b2595eecc96e2fb1730af53
My PC <alex.petkov606@gmail.com>
2023-01-11 17:08:44 +0200

    fds

---
+++ src/Advanced/StacksAndQueues/BasicQueueOperations/BasicQueueOperations.java
@@ -0,0 +1,11 @@
+package Advanced.StacksAndQueues.BasicQueueOperations;
+
+import java.util.Scanner;
+
+public class BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
