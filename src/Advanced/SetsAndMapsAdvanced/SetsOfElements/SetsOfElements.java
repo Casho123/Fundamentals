38469413bcd26ccd5de52c4f53999b6f980ba41c
My PC <alex.petkov606@gmail.com>
2023-01-12 23:25:40 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/SetsOfElements/SetsOfElements.java
@@ -0,0 +1,10 @@
+package Advanced.SetsAndMapsAdvanced.SetsOfElements;
+
+import java.util.Scanner;
+
+public class SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
--- src/Advanced/SetsAndMapsAdvanced/UniqueUsernames/UniqueUsernames.java
+++ src/Advanced/SetsAndMapsAdvanced/UniqueUsernames/UniqueUsernames.java
@@ -1,10 +1,22 @@
 package Advanced.SetsAndMapsAdvanced.UniqueUsernames;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UniqueUsernames {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        Set<String> set = new LinkedHashSet<>();
+        for (int i = 0; i < n; i++) {
+            String current = scan.nextLine();
+            set.add(current);
+        }
+        for (String s : set) {
+            System.out.println(s);
+        }
 
     }
 }
