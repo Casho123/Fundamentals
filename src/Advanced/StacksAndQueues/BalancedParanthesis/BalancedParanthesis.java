06977b6f46e29668b1cca8af58bfa1411f6101bb
My PC <alex.petkov606@gmail.com>
2023-01-12 13:07:03 +0200

    fds

--- src/Advanced/StacksAndQueues/BalancedParanthesis/BalancedParanthesis.java
+++ src/Advanced/StacksAndQueues/BalancedParanthesis/BalancedParanthesis.java
@@ -1,10 +1,36 @@
 package Advanced.StacksAndQueues.BalancedParanthesis;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BalancedParanthesis {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        char[] arr = scan.nextLine().toCharArray();
+
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            stack.push(arr[i]);
+        }
+        boolean isEqual = true;
+        while(!stack.isEmpty()) {
+            if (stack.pop() != stack.poll()) {
+                isEqual = false;
+                break;
+
+
+            }
+        }
+        if (isEqual) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
 
 
     }
