6b948284fee5aced9e38a70352d175cde63a3792
My PC <alex.petkov606@gmail.com>
2023-01-12 23:32:05 +0200

    fds

--- src/Advanced/SetsAndMapsAdvanced/SetsOfElements/SetsOfElements.java
+++ src/Advanced/SetsAndMapsAdvanced/SetsOfElements/SetsOfElements.java
@@ -1,11 +1,31 @@
 package Advanced.SetsAndMapsAdvanced.SetsOfElements;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class SetsOfElements {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        Set<Integer> first = new LinkedHashSet<>();
+        Set<Integer> second = new LinkedHashSet<>();
+
+        for (int i = 0; i < numbers[0]; i++) {
+            int currentNum = Integer.parseInt(scan.nextLine());
+            first.add(currentNum);
+        }
+        for (int i = 0; i < numbers[1]; i++) {
+            int currentNum = Integer.parseInt(scan.nextLine());
+            second.add(currentNum);
+        }
+        for (Integer current : first) {
+            if (second.contains(current)) {
+                System.out.print(current + " ");
+            }
+        }
 
 
     }
