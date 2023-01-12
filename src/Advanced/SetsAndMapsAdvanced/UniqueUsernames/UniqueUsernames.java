13b391597411ee6b9fe638c81ff707c23715c9e4
My PC <alex.petkov606@gmail.com>
2023-01-12 23:22:37 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/UniqueUsernames/UniqueUsernames.java
@@ -0,0 +1,10 @@
+package Advanced.SetsAndMapsAdvanced.UniqueUsernames;
+
+import java.util.Scanner;
+
+public class UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
