d9c2a48beb2af45cb9f80b8579dda03e324dd313
My PC <alex.petkov606@gmail.com>
2023-01-12 18:52:51 +0200

    fds

--- src/Advanced/StacksAndQueues/RecursiveFibonacci/RecursiveFibonacci.java
+++ src/Advanced/StacksAndQueues/RecursiveFibonacci/RecursiveFibonacci.java
@@ -7,17 +7,17 @@ public class RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayDeque<Long> fibonacci = new ArrayDeque<>();
-        long N = Integer.parseInt(scan.nextLine());
-        System.out.println(getFibonacci(fibonacci, N));
+        long n = Integer.parseInt(scan.nextLine());
+        System.out.println(getFibonacci(fibonacci, n));
     }
 
-    private static Long getFibonacci(ArrayDeque<Long> fibonacci, Long N) {
-        if (N < 2) {
+    private static Long getFibonacci(ArrayDeque<Long> fibonacci, Long n) {
+        if (n < 2) {
             return 1L;
         } else {
             fibonacci.offer(0L);
             fibonacci.offer(1L);
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < n; i++) {
                 long Sum = fibonacci.poll() + fibonacci.peek();
                 fibonacci.offer(Sum);
             }
