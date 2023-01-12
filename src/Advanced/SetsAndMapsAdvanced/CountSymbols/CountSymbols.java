88d9b7f74b9f852346514ac5e52bf469fc7943c2
My PC <alex.petkov606@gmail.com>
2023-01-12 23:39:49 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/CountSymbols/CountSymbols.java
@@ -0,0 +1,11 @@
+package Advanced.SetsAndMapsAdvanced.CountSymbols;
+
+import java.util.Scanner;
+
+public class CountSymbols {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
