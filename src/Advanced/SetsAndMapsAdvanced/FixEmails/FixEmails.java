ddd1985222a2894a3cdca16c9f096b3d3c83a1e3
My PC <alex.petkov606@gmail.com>
2023-01-13 02:15:04 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/FixEmails/FixEmails.java
@@ -0,0 +1,11 @@
+package Advanced.SetsAndMapsAdvanced.FixEmails;
+
+import java.util.Scanner;
+
+public class FixEmails {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
