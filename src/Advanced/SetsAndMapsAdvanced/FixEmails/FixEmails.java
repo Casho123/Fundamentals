83306de7f354d8305dbda736a9e7344585161204
My PC <alex.petkov606@gmail.com>
2023-01-13 02:21:29 +0200

    fds

--- src/Advanced/SetsAndMapsAdvanced/FixEmails/FixEmails.java
+++ src/Advanced/SetsAndMapsAdvanced/FixEmails/FixEmails.java
@@ -1,10 +1,29 @@
 package Advanced.SetsAndMapsAdvanced.FixEmails;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class FixEmails {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Map<String, String> people = new LinkedHashMap<>();
+
+        while(true) {
+            String input = scan.nextLine();
+            if (input.equals("stop")) {
+                break;
+            }
+            String email = scan.nextLine();
+            if (!email.endsWith("com") && !email.endsWith("uk") && !email.endsWith("us")) {
+                people.putIfAbsent(input, email);
+            }
+        }
+
+        for (Map.Entry<String, String> entry : people.entrySet()) {
+            System.out.printf("%s -> %s\n", entry.getKey(), entry.getValue());
+        }
 
 
     }
