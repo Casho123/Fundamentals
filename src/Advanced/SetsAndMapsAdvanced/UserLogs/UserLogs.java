a3a7304eeb7449cc484899d72bab34f494166071
My PC <alex.petkov606@gmail.com>
2023-01-13 15:20:10 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/UserLogs/UserLogs.java
@@ -0,0 +1,11 @@
+package Advanced.SetsAndMapsAdvanced.UserLogs;
+
+import java.util.Scanner;
+
+public class UserLogs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
