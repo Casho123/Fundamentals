03bd23f45af6e8c2ba485d08213ca1544d18573a
My PC <alex.petkov606@gmail.com>
2023-01-13 02:22:14 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/HandsOfCards/HandsOfCards.java
@@ -0,0 +1,11 @@
+package Advanced.SetsAndMapsAdvanced.HandsOfCards;
+
+import java.util.Scanner;
+
+public class HandsOfCards {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
