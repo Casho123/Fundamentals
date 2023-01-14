a4580997ef774d8e02c7c7604e6b36eb18b2c4e9
My PC <alex.petkov606@gmail.com>
2023-01-14 20:44:57 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/PopulationCounter/PopulationCounter.java
@@ -0,0 +1,10 @@
+package Advanced.SetsAndMapsAdvanced.PopulationCounter;
+
+import java.util.Scanner;
+
+public class PopulationCounter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
