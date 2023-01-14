6cf4dd9f2af5ca639aaa823aa140654d53034861
My PC <alex.petkov606@gmail.com>
2023-01-14 20:50:02 +0200

    fds

--- src/Advanced/SetsAndMapsAdvanced/PopulationCounter/PopulationCounter.java
+++ src/Advanced/SetsAndMapsAdvanced/PopulationCounter/PopulationCounter.java
@@ -1,10 +1,29 @@
 package Advanced.SetsAndMapsAdvanced.PopulationCounter;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PopulationCounter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Map<String, Map<String, Integer>> countries = new LinkedHashMap<>();
+
+        while(true) {
+            String input = scan.nextLine();
+
+            if (input.equals("report")) {
+                break;
+            }
+
+            String[] elements = input.split("\\|");
+            String city = elements[0];
+            String country = elements[1];
+            int population = Integer.parseInt(elements[2]);
+
+            countries.putIfAbsent(country, new LinkedHashMap<>());
+        }
 
     }
 }
