91b094f2ada38481e7fb9d38e2a4f1fd2e18ef86
My PC <alex.petkov606@gmail.com>
2023-01-16 21:12:22 +0200

    fds

---
+++ src/Advanced/SetsAndMapsAdvanced/SrpskoUnleashed/SrpskoUnleashed.java
@@ -0,0 +1,10 @@
+package Advanced.SetsAndMapsAdvanced.SrpskoUnleashed;
+
+import java.util.Scanner;
+
+public class SrpskoUnleashed {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
