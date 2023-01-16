890aed411e890e14b9f52ac061a2e1a901242e70
My PC <alex.petkov606@gmail.com>
2023-01-16 21:28:02 +0200

    fds

--- src/Advanced/SetsAndMapsAdvanced/SrpskoUnleashed/SrpskoUnleashed.java
+++ src/Advanced/SetsAndMapsAdvanced/SrpskoUnleashed/SrpskoUnleashed.java
@@ -6,5 +6,19 @@ public class SrpskoUnleashed {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        while(true) {
+            String input = scan.nextLine();
+            if (input.equals("End")) {
+                break;
+            }
+            if (input.contains(" @")) {
+                String[] elements = input.split(" @");
+                String singer = elements[0];
+
+
+            }
+
+        }
+
     }
 }
