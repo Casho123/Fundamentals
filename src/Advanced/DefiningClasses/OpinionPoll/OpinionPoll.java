cabb531092b973e8f004abb09946bbed1ee2b549
My PC <alex.petkov606@gmail.com>
2023-01-18 21:34:22 +0200

    fds

---
+++ src/Advanced/DefiningClasses/OpinionPoll/OpinionPoll.java
@@ -0,0 +1,11 @@
+package Advanced.DefiningClasses.OpinionPoll;
+
+import java.util.Scanner;
+
+public class OpinionPoll {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
