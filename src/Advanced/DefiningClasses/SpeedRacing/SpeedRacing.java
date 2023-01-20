2989d85b01c503775fd372dcc4f4eaf978653419
My PC <alex.petkov606@gmail.com>
2023-01-20 23:06:50 +0200

    fds

---
+++ src/Advanced/DefiningClasses/SpeedRacing/SpeedRacing.java
@@ -0,0 +1,11 @@
+package Advanced.DefiningClasses.SpeedRacing;
+
+import java.util.Scanner;
+
+public class SpeedRacing {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
