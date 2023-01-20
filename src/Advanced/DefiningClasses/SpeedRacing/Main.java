8fadc6c8ebe9f466db0a3cd74dc1cddcf08f20ae
My PC <alex.petkov606@gmail.com>
2023-01-20 23:32:18 +0200

    fds

--- src/Advanced/DefiningClasses/SpeedRacing/Main.java
+++ src/Advanced/DefiningClasses/SpeedRacing/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class SpeedRacing {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
