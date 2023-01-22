4ca0d37be359633f40467168fd359aa7379aa254
My PC <alex.petkov606@gmail.com>
2023-01-22 15:08:21 +0200

    fds

---
+++ src/Advanced/DefiningClasses/Google/Main.java
@@ -0,0 +1,10 @@
+package Advanced.DefiningClasses.Google;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
