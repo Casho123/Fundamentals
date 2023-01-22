f97a48f2d79b0bf2d70479b20c5895814c0c71ac
My PC <alex.petkov606@gmail.com>
2023-01-23 01:34:16 +0200

    fds

--- src/Advanced/DefiningClasses/CatLady/Main.java
+++ src/Advanced/DefiningClasses/CatLady/Main.java
@@ -10,9 +10,39 @@ public class Main {
 
         List<Cat> cats = new ArrayList<>();
 
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("End")) {
+                break;
+            }
+            String[] elements = input.split("\\s+");
+            String breed = elements[0];
+            String name = elements[1];
+            double value = Double.parseDouble(elements[2]);
+            switch (breed) {
+                case "Siamese":
+                    Siamese siamese = new Siamese(name, value);
+                    cats.add(siamese);
+                    break;
+                case "Cymric":
+                    Cymric cymric = new Cymric(name, value);
+                    cats.add(cymric);
+                    break;
+                case "StreetExtraordinaire":
+                    StreetExtraordinaire street = new StreetExtraordinaire(name, value);
+                    cats.add(street);
+                    break;
+            }
+        }
+        String catName = scan.nextLine();
+        Cat cat = null;
+        for (Cat currentCat : cats) {
+            if (currentCat.getName().equals(catName)) {
+                cat = currentCat;
+                break;
+            }
 
-
-
-
+        }
+        System.out.println(cat);
     }
 }
