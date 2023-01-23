512c9fa6e7d3d85a36a935e7e40e7df499c6d8db
My PC <alex.petkov606@gmail.com>
2023-01-24 01:01:46 +0200

    fds

---
+++ src/Advanced/Generics/GenericBoxOfInteger/Main.java
@@ -0,0 +1,9 @@
+package Advanced.Generics.GenericBoxOfInteger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
