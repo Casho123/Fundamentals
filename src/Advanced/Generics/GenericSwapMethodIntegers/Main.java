05d7a1226ce3625dabb8d706e1c536289edd3eac
My PC <alex.petkov606@gmail.com>
2023-01-26 22:32:11 +0200

    fds

--- src/Advanced/Generics/GenericSwapMethodIntegers/Main.java
+++ src/Advanced/Generics/GenericSwapMethodIntegers/Main.java
@@ -1,9 +1,29 @@
 package Advanced.Generics.GenericSwapMethodIntegers;
 
+import Advanced.Generics.GenericSwapMethodStrings.Box;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Box<Integer> box = new Box<>();
+
+        int n = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < n; i++) {
+            String current = scan.nextLine();
+            box.addValue(Integer.parseInt(current));
+
+        }
+        int[] indexes = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int index1 = indexes[0];
+        int index2 = indexes[1];
+        box.swap(index1, index2);
+
+        System.out.println(box);
+
+
     }
 }
