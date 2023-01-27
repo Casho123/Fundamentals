d75161ddb7aa54571f80909d4ccf3b60f5f5e91a
My PC <alex.petkov606@gmail.com>
2023-01-27 12:39:36 +0200

    fds

---
+++ src/Advanced/Generics/CustomList/Main.java
@@ -0,0 +1,10 @@
+package Advanced.Generics.CustomList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
