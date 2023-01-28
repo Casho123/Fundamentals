a288f978a96ce31b7853ae287ad37709b48791c7
My PC <alex.petkov606@gmail.com>
2023-01-28 22:46:17 +0200

    fds

---
+++ src/Advanced/IteratorsAndComparators/ListIterator/Main.java
@@ -0,0 +1,10 @@
+package Advanced.IteratorsAndComparators.ListIterator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
