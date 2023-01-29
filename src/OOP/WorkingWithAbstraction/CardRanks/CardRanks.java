6398285fcf43fb4cf1353a746f18e47b845ff2ec
My PC <alex.petkov606@gmail.com>
2023-01-29 21:04:41 +0200

    fds

---
+++ src/OOP/WorkingWithAbstraction/CardRanks/CardRanks.java
@@ -0,0 +1,17 @@
+package OOP.WorkingWithAbstraction.CardRanks;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
--- src/OOP/WorkingWithAbstraction/CardRanks/Main.java
+++ src/OOP/WorkingWithAbstraction/CardRanks/Main.java
@@ -1,4 +1,16 @@
 package OOP.WorkingWithAbstraction.CardRanks;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        System.out.println(input + ":");
+        CardRanks[] ranks = CardRanks.values();
+        for (CardRanks rank : ranks) {
+            System.out.printf("Ordinal value: %d; Name value: %s\n", rank.ordinal(), rank.name());
+        }
+    }
 }
