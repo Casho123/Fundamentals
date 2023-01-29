98d0af6b843bd3174226a3e41d3b8830012cc64c
My PC <alex.petkov606@gmail.com>
2023-01-29 20:56:28 +0200

    fds

---
+++ src/OOP/WorkingWithAbstraction/CardSuit.java
@@ -0,0 +1,10 @@
+package OOP.WorkingWithAbstraction;
+
+import java.util.Scanner;
+
+public class CardSuit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
