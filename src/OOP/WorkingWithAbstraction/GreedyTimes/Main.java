e2a301c5a2a97cfaa7405f52dc810e9c5dcf7d68
My PC <alex.petkov606@gmail.com>
2023-01-31 00:13:53 +0200

    fds

--- src/OOP/WorkingWithAbstraction/GreedyTimes/Main.java
+++ src/OOP/WorkingWithAbstraction/GreedyTimes/Main.java
@@ -14,15 +14,39 @@ public class Main {
 
 
         String[] items = scan.nextLine().split("\\s+");
+        double currentCapacity = 0;
 
         for (int i = 0; i < items.length; i+=2) {
             String item = items[i];
             int quantity = Integer.parseInt(items[i+1]);
+            if (currentCapacity + quantity <= capacity) {
+                if (item.equals("Gold")) {
+                    stuff.putIfAbsent("Gold", new LinkedHashMap<>());
+                    if (!stuff.get("Gold").containsKey("Gold")) {
+                        stuff.get("Gold").put("Gold", quantity);
+                    } else {
+                        stuff.get("Gold").put("Gold", stuff.get("Gold").get("Gold") + quantity);
+                    }
+                }
+                if (item.toLowerCase().endsWith("gem")) {
+                    stuff.putIfAbsent("Gem", new LinkedHashMap<>());
+                    if (!stuff.get("Gem").containsKey(item)) {
+                        stuff.get("Gem").put(item, quantity);
+                    } else {
+                        stuff.get("Gem").put(item, stuff.get("Gem").get(item) + quantity);
+                    }
+                }
+
+
+
+            }
+
 
 
 
 
         }
+        System.out.println();
 
 
 
