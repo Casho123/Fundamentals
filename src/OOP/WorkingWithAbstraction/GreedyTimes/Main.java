b9e8e38ce930e1ee3802a8fab6ea549aef6a545e
My PC <alex.petkov606@gmail.com>
2023-01-30 23:45:47 +0200

    fds

--- src/OOP/WorkingWithAbstraction/GreedyTimes/Main.java
+++ src/OOP/WorkingWithAbstraction/GreedyTimes/Main.java
@@ -1,5 +1,8 @@
 package OOP.WorkingWithAbstraction.GreedyTimes;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +10,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         double capacity = Double.parseDouble(scan.nextLine());
+        Map<String, List<String>> stuff = new LinkedHashMap<>();
 
 
         String[] input = scan.nextLine().split("\\s+");
