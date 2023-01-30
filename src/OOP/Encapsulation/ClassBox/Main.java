cbb81ea0cd912aefde572e44a2ddedd7b9405d42
My PC <alex.petkov606@gmail.com>
2023-01-31 00:43:04 +0200

    fds

--- src/OOP/Encapsulation/ClassBox/Main.java
+++ src/OOP/Encapsulation/ClassBox/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        double length = Double.parseDouble(scan.nextLine());
+        double width = Double.parseDouble(scan.nextLine());
+        double height = Double.parseDouble(scan.nextLine());
+
+        Box box = new Box(length, width, height);
+
+        System.out.printf("Surface Area - %.2f\n", box.calculateSurfaceArea());
+        System.out.printf("Lateral Surface Area - %.2f\n", box.calculateLateralSurfaceArea());
+        System.out.printf("Volume – %.2f\n", box.calculateVolume());
+
 
 
     }
