c88bcde6c4e786b5cc3c4cf74b70681be7d8e4d7
My PC <alex.petkov606@gmail.com>
2023-01-30 23:21:26 +0200

    fds

---
+++ src/OOP/WorkingWithAbstraction/GreedyTimes/Main.java
@@ -0,0 +1,11 @@
+package OOP.WorkingWithAbstraction.GreedyTimes;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
