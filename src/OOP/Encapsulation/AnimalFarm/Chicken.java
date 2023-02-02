d423d050b167c0c3f25681706216c60261c72a12
My PC <alex.petkov606@gmail.com>
2023-02-02 22:47:31 +0200

    fds

--- src/OOP/Encapsulation/AnimalFarm/Chicken.java
+++ src/OOP/Encapsulation/AnimalFarm/Chicken.java
@@ -12,11 +12,40 @@ public class Chicken {
     }
 
     private void setName(String name) {
+        if (name == null || name.trim().equals("")) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
         this.name = name;
     }
 
     private void setAge(int age) {
+        if (age < 0 || age > 15 ) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
         this.age = age;
+    }
+
+    public double productPerDay() {
+        return calculateProductPerDay();
+    }
+
+
+
+    private double calculateProductPerDay() {
+        double eggsPerDay = 0;
+        if (this.age <= 5) {
+            eggsPerDay = 2;
+        } else if (this.age <= 11) {
+            eggsPerDay = 1;
+        } else {
+            eggsPerDay = 0.75;
+        }
+        return eggsPerDay;
+    }
+
+    @Override
+    public String toString() {
+            return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, productPerDay());
     }
 }
 
--- src/OOP/Encapsulation/AnimalFarm/Main.java
+++ src/OOP/Encapsulation/AnimalFarm/Main.java
@@ -6,5 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        String name = scan.nextLine();
+        int age = Integer.parseInt(scan.nextLine());
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.println(chicken);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
