ccb60270c1f66273fe4195fc8a447d2fbb2f33a8
My PC <alex.petkov606@gmail.com>
2023-02-03 14:04:51 +0200

    fds

--- src/OOP/Encapsulation/ShoppingSpree/Person.java
+++ src/OOP/Encapsulation/ShoppingSpree/Person.java
@@ -1,4 +1,35 @@
 package OOP.Encapsulation.ShoppingSpree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+
+    private String name;
+    private double money;
+    private List<Product> products = new ArrayList<>();
+
+    public Person(String name, double money) {
+        setName(name);
+        setMoney(money);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().equals("")) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
 }
