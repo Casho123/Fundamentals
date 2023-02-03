9979cea92a7796eee8752c561ded1defb3dc6420
My PC <alex.petkov606@gmail.com>
2023-02-03 13:58:48 +0200

    fds

---
+++ src/OOP/Encapsulation/ShoppingSpree/Person.java
@@ -0,0 +1,4 @@
+package OOP.Encapsulation.ShoppingSpree;
+
+public class Person {
+}
---
+++ src/OOP/Encapsulation/ShoppingSpree/Product.java
@@ -0,0 +1,28 @@
+package OOP.Encapsulation.ShoppingSpree;
+
+public class Product {
+
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
