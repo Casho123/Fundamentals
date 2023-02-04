bfcda5ccc83f59088077987ad6a0c0bfbedc309a
My PC <alex.petkov606@gmail.com>
2023-02-04 15:25:14 +0200

    fds

---
+++ src/OOP/Encapsulation/PizzaCalories/Pizza.java
@@ -0,0 +1,53 @@
+package OOP.Encapsulation.PizzaCalories;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Pizza {
+
+
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+    public Pizza(String name, int numberOfToppings) {
+        setName(name);
+        setToppings(numberOfToppings);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Dough getDough() {
+        return dough;
+    }
+
+    public List<Topping> getToppings() {
+        return Collections.unmodifiableList(this.toppings);
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    private void setToppings(int numberOfToppings) {
+        if (numberOfToppings > 10 || numberOfToppings < 0) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+        this.toppings = new ArrayList<>(numberOfToppings);
+    }
+
+    public void addTopping(Topping topping) {
+        this.toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        return 0;
+    }
+}
