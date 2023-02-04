c46e63b89d764e950470a2f012a2b06125844232
My PC <alex.petkov606@gmail.com>
2023-02-04 23:36:28 +0200

    fds

--- src/OOP/Encapsulation/PizzaCalories/Main.java
+++ src/OOP/Encapsulation/PizzaCalories/Main.java
@@ -8,6 +8,24 @@ public class Main {
 
         String[] input = scan.nextLine().split("\\s+");
 
+        Pizza pizza = new Pizza(input[1], Integer.parseInt(input[2]));
+        String[] arrDough = scan.nextLine().split("\\s+");
+        Dough dough = new Dough(arrDough[1], arrDough[2], Double.parseDouble(arrDough[3]));
+        pizza.setDough(dough);
+
+        while (true) {
+            String current = scan.nextLine();
+            if (current.equals("END")) {
+                break;
+            }
+            String[] toppingArr = current.split("\\s+");
+            String toppingType = toppingArr[1];
+            double toppingWeight = Double.parseDouble(toppingArr[2]);
+            Topping topping = new Topping(toppingType, toppingWeight);
+            pizza.addTopping(topping);
+        }
+        System.out.printf("%s - %.2f",pizza.getName(), pizza.getOverallCalories() );
+
 
 
     }
