c6be5491e0c4e60b7af6f6fd6e884462192f5d18
My PC <alex.petkov606@gmail.com>
2023-02-08 13:44:37 +0200

    fds

---
+++ src/OOP/Restaurant/Beverage.java
@@ -0,0 +1,21 @@
+package OOP.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product {
+
+    private double milliliters;
+
+
+
+
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+}
---
+++ src/OOP/Restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package OOP.Restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
