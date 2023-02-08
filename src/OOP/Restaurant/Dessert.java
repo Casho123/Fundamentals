67150eab235f2015379adf32a6cb7f9b378c6c5c
My PC <alex.petkov606@gmail.com>
2023-02-08 13:54:33 +0200

    fds

---
+++ src/OOP/Restaurant/Dessert.java
@@ -0,0 +1,13 @@
+package OOP.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Dessert extends Food {
+
+    private double calories;
+
+    public Dessert(String name, BigDecimal price, double grams, double calories) {
+        super(name, price, grams);
+        this.calories = calories;
+    }
+}
