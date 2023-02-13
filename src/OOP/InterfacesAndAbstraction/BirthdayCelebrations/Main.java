fb33989048bf2bcaf3350be9e02ee3f626841526
My PC <alex.petkov606@gmail.com>
2023-02-13 12:02:38 +0200

    fds

--- src/OOP/InterfacesAndAbstraction/BirthdayCelebrations/Main.java
+++ src/OOP/InterfacesAndAbstraction/BirthdayCelebrations/Main.java
@@ -1,7 +1,12 @@
 package OOP.InterfacesAndAbstraction.BirthdayCelebrations;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
 
     }
 }
