f28afe62827da30ea0ebdcaaebe7e8c9da84e6b0
My PC <alex.petkov606@gmail.com>
2023-02-21 00:00:59 +0200

    fds

---
+++ src/OOP/Polymorphism/Vehicles/Bus.java
@@ -0,0 +1,7 @@
+package OOP.Polymorphism.Vehicles;
+
+public class Bus extends Vehicle{
+    protected Bus(double fuelQuantity, double fuelConsumption) {
+        super(fuelQuantity, fuelConsumption);
+    }
+}
