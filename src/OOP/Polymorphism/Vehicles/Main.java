2ba289e47fbfaa2522dde54063e862f5b3fbeaa6
My PC <alex.petkov606@gmail.com>
2023-02-21 00:07:06 +0200

    fds

--- src/OOP/Polymorphism/Vehicles/Main.java
+++ src/OOP/Polymorphism/Vehicles/Main.java
@@ -10,6 +10,7 @@ public class Main {
         int number = Integer.parseInt(scan.nextLine());
         Vehicle car = new Car(Double.parseDouble(elements[1]), Double.parseDouble(elements[2]), Double.parseDouble(elements[3]));
         Vehicle truck = new Truck(Double.parseDouble(tokens[1]), Double.parseDouble(tokens[2]), Double.parseDouble(elements[3]));
+        Vehicle bus = new Bus(Double.parseDouble(tokens[1]), Double.parseDouble(tokens[2]), Double.parseDouble(elements[3]));
 
         for (int i = 0; i < number; i++) {
             String[] input = scan.nextLine().split("\\s+");
