4bf88456419c5b2f51e20f36d43c0a49bd165299
My PC <alex.petkov606@gmail.com>
2023-02-22 18:48:49 +0200

    fds

---
+++ src/OOP/TestingJava/C.java
@@ -0,0 +1,26 @@
+package OOP.TestingJava;
+
+public class C {
+    public static void main(String[] args) {
+        new A();
+        new B();
+    }
+}
+class A {
+    int i = 7;
+    public A() {
+        setI(20);
+        System.out.println("i from A is " + i);
+    }
+    public void setI(int i) {
+        this.i = 2 * i;
+    }
+}
+class B extends A {
+    public B() {
+        System.out.println("i from B is " + i);
+    }
+    public void setI(int i) {
+        this.i = 3 * i;
+    }
+}
