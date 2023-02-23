b3ef685b46bd865cf0fbfb795dc5430c42fcbea8
My PC <alex.petkov606@gmail.com>
2023-02-23 13:23:20 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,9 +3,15 @@ package OOP.TestingJava;
 public class C {
     public static void main(String[] args) {
 
-        GeometricObject object = new GeometricObject();
-        Circle circle = (Circle)object;
+        Fruit fruit = new GoldenDelicious();
+        Orange orange = new Orange();
 
+        System.out.println(fruit instanceof Fruit);
+        System.out.println(fruit instanceof Orange);
+        System.out.println(fruit instanceof Apple);
+        System.out.println(fruit instanceof GoldenDelicious);
+        System.out.println(fruit instanceof McIntosh);
+        System.out.println(orange instanceof Orange);
     }
 }
 
