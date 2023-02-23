f855518084e2c18997cbfaef1842e1233b307aa9
My PC <alex.petkov606@gmail.com>
2023-02-23 14:52:41 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,15 +3,8 @@ package OOP.TestingJava;
 public class C {
     public static void main(String[] args) {
 
-        Fruit fruit = new GoldenDelicious();
-        Orange orange = new Orange();
-
-        System.out.println(fruit instanceof Fruit);
-        System.out.println(fruit instanceof Orange);
-        System.out.println(fruit instanceof Apple);
-        System.out.println(fruit instanceof GoldenDelicious);
-        System.out.println(fruit instanceof McIntosh);
-        System.out.println(orange instanceof Orange);
+        Object fruit = new Fruit();
+        Object apple = (Apple)fruit;
     }
 }
 
