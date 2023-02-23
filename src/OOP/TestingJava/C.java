04594053f39ddd54accd9509765dd739f03267d4
My PC <alex.petkov606@gmail.com>
2023-02-23 14:57:13 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,8 +3,11 @@ package OOP.TestingJava;
 public class C {
     public static void main(String[] args) {
 
-        Object fruit = new Fruit();
-        Object apple = (Apple)fruit;
+       Apple apple = new Apple();
+       Apple apple2 = apple;
+
+        System.out.println(apple == apple2);
+        System.out.println(apple.equals(apple2));
     }
 }
 
