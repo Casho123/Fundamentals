95b33fd876ffad2f2b7c78ae94301a5895e291b3
My PC <alex.petkov606@gmail.com>
2023-02-25 11:56:12 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -1,16 +1,29 @@
 package OOP.TestingJava;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class C {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Denver");
-        list.add("Austin");
-        list.add(new java.util.Date());
-        String city = list.get(0);
-        list.set(3, "Dallas");
-        System.out.println(list.get(3));
+
+
     }
+
+    public class A {
+
+       protected int i;
+
+
+       protected void m() {
+
+        }
+    }
+    public class B extends A {
+        public void m1(String[] args) {
+            System.out.println(i);
+            m();
+        }
+    }
+
 }
 
