898758dd88d48b5912820730c38973b188482401
My PC <alex.petkov606@gmail.com>
2023-02-25 11:31:59 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -4,7 +4,7 @@ public class C {
     public static void main(String[] args) {
 
        Apple apple = new Apple();
-       Apple apple2 = apple;
+       Apple apple2 = new Apple();
 
         System.out.println(apple == apple2);
         System.out.println(apple.equals(apple2));
