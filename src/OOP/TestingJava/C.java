25f1ebf6ec6dd26868fce647846c511b7cccd902
My PC <alex.petkov606@gmail.com>
2023-02-27 17:56:13 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -1,19 +1,30 @@
 package OOP.TestingJava;
 
 
-import java.util.Calendar;
-import java.util.Date;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class C {
     public static void main(String[] args) {
 
-        Integer n1 = new Integer(3);
-        Object n2 = new Integer(4);
-        System.out.println(n1.compareTo(n2));
+        Person[] persons = {new Person(3), new Person(4), new Person(1)};
+        Arrays.sort(persons);
 
 
     }
+    static class Person implements Comparable {
+        private int id;
 
+        Person(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public int compareTo(@NotNull Object o) {
+            return 0;
+        }
+    }
 }
 
 
