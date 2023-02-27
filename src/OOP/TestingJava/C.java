b85126b0a85e091fa371356e250ee47b243f90dd
My PC <alex.petkov606@gmail.com>
2023-02-27 15:11:12 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,16 +3,19 @@ package OOP.TestingJava;
 
 public class C {
     public static void main(String[] args) {
-        try {
-            for (int i = 0; i < 2; i++) {
-                System.out.print(i + " ");
-                System.out.println(1 / 0);
-            }
-        }
-        catch (Exception ex) {
-        }
+        System.out.println(1 / 0);
     }
 
+    abstract class A {
+        abstract void unfinished();
+    }
 
+    abstract class B {
+        abstract int unfinished();
+    }
 }
+
+
+
+
 
