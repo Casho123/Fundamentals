5a44f4c1816995fdc972145e45e3db0ad95bda53
My PC <alex.petkov606@gmail.com>
2023-02-27 16:43:36 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,16 +3,13 @@ package OOP.TestingJava;
 
 public class C {
     public static void main(String[] args) {
-        System.out.println(1 / 0);
+
+
+        Number x = 3;
+        System.out.println(x.intValue());
+        System.out.println(((Integer) x).compareTo(4));
     }
 
-    abstract class A {
-        abstract void unfinished();
-    }
-
-    abstract class B {
-        abstract int unfinished();
-    }
 }
 
 
