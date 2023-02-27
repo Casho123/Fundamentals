54018e18599cefcb6aa3edf2616286f151c2ab11
My PC <alex.petkov606@gmail.com>
2023-02-27 17:47:14 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -1,13 +1,20 @@
 package OOP.TestingJava;
 
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class C {
     public static void main(String[] args) {
 
+        Date n = new Date(2013, Calendar.APRIL,1);
 
-        Number x = 3;
-        System.out.println(x.intValue());
-        System.out.println(((Integer) x).compareTo(4));
+        System.out.println(n instanceof Date);
+        System.out.println(n instanceof Object);
+        System.out.println(n instanceof Comparable<?>);
+        String date = "sas";
+
+
     }
 
 }
