1b789d7f7b2cf1a5eeb26d6750d29f87507554fe
My PC <alex.petkov606@gmail.com>
2023-02-27 12:56:35 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -4,15 +4,13 @@ package OOP.TestingJava;
 public class C {
     public static void main(String[] args) {
         try {
-            int value = 50;
-            if (value < 40)
-                throw new Exception("value is too small");
+            for (int i = 0; i < 2; i++) {
+                System.out.print(i + " ");
+                System.out.println(1 / 0);
+            }
         }
         catch (Exception ex) {
-            System.out.println(ex.getMessage());
         }
-        System.out.println("Continue after the catch block");
-
     }
 
 
