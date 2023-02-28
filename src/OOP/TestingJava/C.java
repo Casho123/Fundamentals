4e92a03bb911b86be652a006bff08f2fa4d5f03b
My PC <alex.petkov606@gmail.com>
2023-02-28 15:23:16 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -2,25 +2,27 @@ package OOP.TestingJava;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Scanner;
+
 
 public class C {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("New York");
-        ArrayList<String> list1 = list;
-        ArrayList<String> list2 = (ArrayList<String>)(list.clone());
-        list.add("Atlanta");
-        System.out.println(list == list1);
-        System.out.println(list == list2);
-        System.out.println("list is " + list);
-        System.out.println("list1 is " + list1);
-        System.out.println("list2.get(0) is " + list2.get(0));
-        System.out.println("list2.size() is " + list2.size());
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a nonnegative integer: ");
+        int n = input.nextInt();
 
+        // Display factorial
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
 
+    /**
+     * Return the factorial for the specified number
+     */
+    public static long factorial(int n) {
+        if (n == 0) // Base case
+            return 1;
+        else
+            return n * factorial(n - 1); // Recursive call
     }
 
 }
