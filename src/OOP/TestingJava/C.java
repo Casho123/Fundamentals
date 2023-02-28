031a9f657b2f457d0f36f296eaba5699ecf8fc77
My PC <alex.petkov606@gmail.com>
2023-02-28 13:43:58 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -3,18 +3,24 @@ package OOP.TestingJava;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class C {
     public static void main(String[] args) {
-        java.util.Date date = new java.util.Date();
-        java.util.Date date1 = date;
-        java.util.Date date2 = (java.util.Date)(date.clone());
-        System.out.println(date == date1);
-        System.out.println(date == date2);
-        System.out.println(date.equals(date2));
+        ArrayList<String> list = new ArrayList<>();
+        list.add("New York");
+        ArrayList<String> list1 = list;
+        ArrayList<String> list2 = (ArrayList<String>)(list.clone());
+        list.add("Atlanta");
+        System.out.println(list == list1);
+        System.out.println(list == list2);
+        System.out.println("list is " + list);
+        System.out.println("list1 is " + list1);
+        System.out.println("list2.get(0) is " + list2.get(0));
+        System.out.println("list2.size() is " + list2.size());
 
 
     }
