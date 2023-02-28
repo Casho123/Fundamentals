39fc7f1c486ace0f489c110ea9b49e8ad804f659
My PC <alex.petkov606@gmail.com>
2023-02-28 13:07:29 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -4,27 +4,24 @@ package OOP.TestingJava;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class C {
     public static void main(String[] args) {
-
-        Person[] persons = {new Person(3), new Person(4), new Person(1)};
-        Arrays.sort(persons);
+        Calendar calendar = new GregorianCalendar(2013, 2, 1);
+        Calendar calendar1 = calendar;
+        Calendar calendar2 = (Calendar)calendar.clone();
+        System.out.println("calendar == calendar1 is " +
+                (calendar == calendar1));
+        System.out.println("calendar == calendar2 is " +
+                (calendar == calendar2));
+        System.out.println("calendar.equals(calendar2) is " +
+                calendar.equals(calendar2));
 
 
     }
-    static class Person implements Comparable {
-        private int id;
 
-        Person(int id) {
-            this.id = id;
-        }
-
-        @Override
-        public int compareTo(@NotNull Object o) {
-            return 0;
-        }
-    }
 }
 
 
