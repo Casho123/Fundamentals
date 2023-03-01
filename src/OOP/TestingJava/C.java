e4dd3e4972cb9f407f625789fb906f16f7623858
My PC <alex.petkov606@gmail.com>
2023-03-01 14:33:55 +0200

    fds

--- src/OOP/TestingJava/C.java
+++ src/OOP/TestingJava/C.java
@@ -1,14 +1,11 @@
 package OOP.TestingJava;
 
 
-import java.util.ArrayList;
-import java.util.Date;
-
 public class C {
     public static void main(String[] args) {
          Integer[] integers = {1, 2, 3, 4, 5};
          String[] strings = {"London", "Paris", "New York", "Austin"};
-        C.print(integers);
+        C.<Integer>print(integers);
         C.print(strings);
         }
 
